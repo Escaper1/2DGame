4afdb6ca5781754f322f1a2a946316f2f1bc45bb
Stefan Iordachi <iordachi.stefan@yahoo.com>
2023-06-07 16:49:57 +0200

    Idle and Walking animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,12 +14,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] int movementSpeed;
 
     private Vector2 movementInput;
-        
+
+    private Animator playerAnimator;
+
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
+            
+        playerAnimator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -37,7 +42,28 @@ public class PlayerController : MonoBehaviour
 
         
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        
         weaponsArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if(mousePosition.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            weaponsArm.localScale = new Vector3(-1f, -1f, 1f);
+        } else
+        {
+            transform.localScale = Vector3.one;
+            weaponsArm.localScale = Vector3.one;
+
+        }
+
+        if(movementInput != Vector2.zero)
+        {
+            playerAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalking", false);
+        }
 
     }
 }
